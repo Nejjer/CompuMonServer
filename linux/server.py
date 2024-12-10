c87e4b4e6d9b0278dd029e2b9e20759dcbf02436
Васеньков Артемий <a.vasenkov@skbkontur.ru>
2024-12-10 22:20:07 +0500

    Переписал сервер на FastAPI

--- linux/server.py
+++ linux/server.py
@@ -1,11 +1,12 @@
-from flask import Flask, jsonify
-from flask_cors import CORS  # Импортируем CORS
+import threading
+import time
+from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+from pydantic import BaseSettings
 from getCpuTemp import get_cpu_temperature_inxi
 from getMemory import get_memory_info
 from getCpuUsage import get_cpu_usage
 from getFanSpeed import get_fan_speed_inxi
-import threading
-import time
 
 # Глобальные переменные для хранения результатов
 cpu_usage_per_core = None
@@ -27,7 +28,6 @@ def update_cpu_temperature():
         print(f"Обновление температуры процессора: {avg_temperature}")
         time.sleep(0.5)
 
-
 # Запускаем потоки для обновления данных
 thread_cpu_usage = threading.Thread(target=update_cpu_usage, daemon=True)
 thread_cpu_temperature = threading.Thread(target=update_cpu_temperature, daemon=True)
@@ -35,32 +35,51 @@ thread_cpu_temperature = threading.Thread(target=update_cpu_temperature, daemon=
 thread_cpu_usage.start()
 thread_cpu_temperature.start()
 
-app = Flask(__name__)
-CORS(app)  # Разрешаем все CORS-запросы
+# Конфигурация приложения через Pydantic Settings
+class Settings(BaseSettings):
+    host: str = "0.0.0.0"  # Адрес, на котором запускается приложение
+    port: int = 5000       # Порт, на котором запускается приложение
 
-@app.route('/api/getPcStatus', methods=['GET'])
-def get_cpu_status():
+    class Config:
+        env_file = ".env"  # Поддержка загрузки конфигурации из файла .env
 
+settings = Settings()
+
+# Создание FastAPI приложения
+app = FastAPI()
+
+# Разрешаем CORS-запросы
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],  # Разрешаем запросы с любых доменов
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+@app.get("/api/getPcStatus")
+def get_pc_status():
     memory_info = get_memory_info()
     fan_info = get_fan_speed_inxi()
-    # Формируем ответ в заданном формате
 
+    # Формируем ответ в заданном формате
     pc_status = {
         "cpu": {
             "load": cpu_usage_per_core,
             "temperature": avg_temperature,
-            'fanSpeed': fan_info
+            "fanSpeed": fan_info,
         },
         "ram": {
-            "total": memory_info['total_memory'],
-            "used": memory_info['used_memory']
-        }
+            "total": memory_info["total_memory"],
+            "used": memory_info["used_memory"],
+        },
     }
 
-    print('Запрос на получение инфы о сервере')
+    print("Запрос на получение информации о сервере")
     print(pc_status)
 
-    return jsonify(pc_status)
+    return pc_status
 
-if __name__ == '__main__':
-    app.run(host='192.168.1.119', port=5000)
+if __name__ == "__main__":
+    import uvicorn
+    uvicorn.run(app, host=settings.host, port=settings.port)
---
+++ win/server.py
@@ -0,0 +1,79 @@
+from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+from pydantic import BaseSettings
+import clr
+import os
+
+from winGetPCInfo import get_gpu_load, get_ram_info, get_cpu_load_and_temperature
+
+# Импортируем библиотеку LibreHardwareMonitor
+current_dir = os.path.dirname(os.path.abspath(__file__))
+library_path = os.path.join(current_dir, '../lib', 'LibreHardwareMonitorLib.dll')
+clr.AddReference(library_path)
+from LibreHardwareMonitor.Hardware import Computer, HardwareType, SensorType
+
+# Инициализация объекта Computer
+computer = Computer()
+computer.IsCpuEnabled = True  # Включаем мониторинг процессора
+computer.IsGpuEnabled = True  # Включаем мониторинг видеокарты
+computer.IsMemoryEnabled = True  # Включаем мониторинг памяти
+computer.IsMotherboardEnabled = True  # Включаем мониторинг материнской платы
+computer.Open()
+
+# Конфигурация приложения через Pydantic Settings
+class Settings(BaseSettings):
+    host: str = "0.0.0.0"  # Адрес, на котором запускается приложение
+    port: int = 5000       # Порт, на котором запускается приложение
+
+    class Config:
+        env_file = ".env"  # Поддержка загрузки конфигурации из файла .env
+
+settings = Settings()
+
+# Создание FastAPI приложения
+app = FastAPI()
+
+# Разрешаем CORS-запросы
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],  # Разрешаем запросы с любых доменов
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+@app.get("/api/getPcStatus")
+def get_pc_status():
+    gpu_load, gpu_temperature, gpu_memory_total, gpu_memory_used, gpu_fan_speeds = get_gpu_load(computer, HardwareType, SensorType)
+    cpu_load, cpu_temperature, fan_speed = get_cpu_load_and_temperature(computer, HardwareType, SensorType)
+    ram_total, ram_used = get_ram_info(computer, HardwareType, SensorType)
+
+    pc_status = {
+        "cpu": {
+            "load": round(cpu_load),
+            "temperature": round(cpu_temperature),
+            "fanSpeed": round(fan_speed),
+        },
+        "gpu": {
+            "load": gpu_load,
+            "temperature": gpu_temperature,
+            "memory": {
+                "total": round(gpu_memory_total / 1024),
+                "used": round(gpu_memory_used / 1024),
+            },
+            "fanSpeed": gpu_fan_speeds,
+        },
+        "ram": {
+            "total": round(ram_total),
+            "used": round(ram_used),
+        },
+    }
+
+    print("Запрос на получение информации о сервере")
+    print(pc_status)
+
+    return pc_status
+
+if __name__ == "__main__":
+    import uvicorn
+    uvicorn.run(app, host=settings.host, port=settings.port)
